318edaae078e143dfb7afbccad635bd301b7fe87
kthss01 <kthss01@naver.com>
2021-08-17 04:41:11 +0900

    sql문 작성
    test branch에서 sql문 작성

---
+++ sql.sql
@@ -0,0 +1,9 @@
+-- sql문을 test branch에서 작성하고 있습니다
+SELECT * FROM USER;
+
+INSERT INTO USERS VALUES (USERID, USERPWD, USERNAME, USEREMAIL);
+
+UPDATE USERS SET (USERNAME = 'test') WHERE USERID = kth;
+
+-- sql 문 작성을 완료했습니다.
+
